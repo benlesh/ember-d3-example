fa00a058d5a53f50893539c54c3117ebdaba2085
Ben Lesh <blesh@netflix.com>
2015-01-22 13:55:16 -0800

    add data refresh

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,4 +4,13 @@ export default Ember.ObjectController.extend({
 	width: 400,
 
 	height: 300,
+
+	actions: {
+		getNewData: function(){
+			this.set('model.data', d3.range(100).map(n => ({ 
+				x: n, 
+				y: Math.floor(Math.random() * 100) 
+			})));
+		}
+	}
 });
